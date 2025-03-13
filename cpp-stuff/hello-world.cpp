de6836c8f2e2db49dec34fbca99c2835a61532cc
Carter <colandry05@gmail.com>
2025-03-13 10:40:33 -0500

    Moved pygame-practice to Personal Projects. Also added cpp-stuff.

---
+++ cpp-stuff/hello-world.cpp
@@ -0,0 +1,6 @@
+#include <iostream> //iostream is a header file that contains basic functions for input and output (in python, this is similar to imports)
+
+int main(){
+    std::cout << "I like pizza!" << std::endl; //This means "standard character output"
+    std::cout << "It's really good!" << std::endl;
+    return 0;
